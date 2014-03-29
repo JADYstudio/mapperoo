f1e59ce1df3b9f419e0a1e70b58b554604de8873
DiNear <howdidyougetthisemail123123@gmail.com>
2014-03-29 06:09:32 -0400

    Update mapperoo.js

--- mapperoo.js
+++ mapperoo.js
@@ -1,6 +1,8 @@
 var uWaterloo = new google.maps.LatLng(43.4689, -80.5400);
 var siberia = new google.maps.LatLng(60, 105);
+var MeetUp_APIKEY = "46d3d10574c5c51716b1f3b747d425a";
 var map;
+var markers = [];
 var geolocate_on = new Boolean();
 
 function initialize() {
@@ -38,7 +40,7 @@ function initialize() {
 	var mapOptions = {
 		zoom: 15,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		//center: uWaterloo,
+		center: uWaterloo,
 
 		disableDefaultUI: true,
 
@@ -58,19 +60,19 @@ function initialize() {
 	};
 
 	// Try Geolocation
-	if(navigator.geolocation) {
-		geolocate_on = true;
-		navigator.geolocation.getCurrentPosition(function(position) {
-			initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-			map.setCenter(initialLocation);
-		}, function() {
-			handleNoGeolocation(geolocate_on);
-		});
-	} else {
-		geolocate_on = false;
-		// Browser doesn't support Geolocation
-		handleNoGeolocation(geolocate_on);
-	}
+	// if(navigator.geolocation) {
+	// 	geolocate_on = true;
+	// 	navigator.geolocation.getCurrentPosition(function(position) {
+	// 		initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+	// 		map.setCenter(initialLocation);
+	// 	}, function() {
+	// 		handleNoGeolocation(geolocate_on);
+	// 	});
+	// } else {
+	// 	geolocate_on = false;
+	// 	// Browser doesn't support Geolocation
+	// 	handleNoGeolocation(geolocate_on);
+	// }
 
 	// Places the Map in the desired 'div'
 	map = new google.maps.Map(document.getElementById('map-canvas'),
@@ -84,9 +86,6 @@ function initialize() {
 	/*google.maps.event.addListener(map, 'click', function(e) {
 		placeMarker(e.latLng, map);
 		});*/
-	
-	// Creates a new list of Markers
-	var markers = [];
 
 	// Create the search box and link it to the UI element.
 	var input = /** @type {HTMLInputElement} */(
@@ -99,25 +98,20 @@ function initialize() {
 	google.maps.event.addListener(searchBox, 'places_changed', function() {
 		var places = searchBox.getPlaces();
 
-		for (var i = 0, marker; marker = markers[i]; i++) {
-			marker.setMap(null);
-		}
-		// For each place, get the icon, place name, and location.
-		markers = [];
-
-		for (var i = 0, place; place = places[i]; i++) {
-			placeMarker(place);
-		}
+		var loc = places[0];
+		//map.setCenter(loc.geometry.location);
+		//placeMarker(loc);
+		//console.log(loc);
+		//console.log(loc.geometry.location.lng());
+		//console.log(loc.geometry.location.lat());
+		deleteMarkers();
+		test(loc.geometry.location.lng(), loc.geometry.location.lat());
+		map.setCenter(loc.geometry.location);
 	});
 
-}
-
-function loadScript() {
-	var script = document.createElement('script');
-	script.type = 'text/javascript';
-	script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-				'callback=initialize';
-	document.body.appendChild(script);
+	setTimeout(function() {
+		console.log(markers);
+	}, 1500);
 }
 
 function placeMarker(place) {
@@ -133,11 +127,56 @@ function placeMarker(place) {
 function handleNoGeolocation(flag){
 	if(flag == true){
 		alert("Geolocation service failed.");
-		map.setCenter(uWaterloo);
+		map.setCenter(siberia);
 	} else {
 		alert("Geolocation not available");
-		map.setCenter(uWaterloo);
+		map.setCenter(siberia);
 	}
 }
+
+function test(lon, lat){
+	var query = "https://api.meetup.com/2/open_events?callback=?&lat="+lat+"&lon="+lon+"&page=30&radius=10&order=distance&key="+MeetUp_APIKEY;
+
+	$.getJSON(query, function (data) {
+		$.each(data.results, function (i, item) {
+			//console.log(item);
+
+			if(item["venue"]){
+				var pos = new google.maps.LatLng(item.venue.lat, item.venue.lon);
+				var marker = new google.maps.Marker({
+					position: pos,
+					map: map,
+					title: item.name
+				});
+				markers.push(marker);
+				map.setZoom(14);
+			}
+		});
+	});
+}
+
+// Sets the map on all markers in the array.
+function setAllMap(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setAllMap(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setAllMap(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
+
 
 google.maps.event.addDomListener(window, 'load', initialize);
