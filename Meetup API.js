ac241d8967363bb7fb07a7e5488d6bbc9aac2765
BunchaNumbers <jacobhuangyuning@msn.com>
2014-03-28 22:29:14 -0400

    Small Changes to index and tests

--- Meetup API.js
+++ Meetup API.js
@@ -3,8 +3,8 @@
 const ApiKey = '405727146c5e62374a697e641e27d';
 
 var test = null;
-
-$(document).ready(function() {
+var url = "https://api.meetup.com/2/open_events.xml?topic=photo&time=,1w&key=405727146c5e62374a697e641e27d";
+/*$(document).ready(function() {
 	var url = "http://api.meetup.com/groups.json/?zip=11211&topic=moms&order=members&key=405727146c5e62374a697e641e27d";
 	
 	$.getJSON(url, function(data){
@@ -12,4 +12,14 @@ $(document).ready(function() {
 	});
 	
 	console.log(test);
+});*/
+
+$(document).ready(function(){
+  $("button").click(function(){
+    $.getJSON(url,function(result){
+      $.each(result, function(i, field){
+        $("div").append(field + " ");
+      });
+    });
+  });
 });
