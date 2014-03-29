19c7e30ea3956ae971e2d0ed7798a84b01257bb5
BunchaNumbers <jacobhuangyuning@msn.com>
2014-03-28 22:44:16 -0400

    Derf

--- Meetup API.js
+++ Meetup API.js
@@ -3,7 +3,7 @@
 const ApiKey = '405727146c5e62374a697e641e27d';
 
 var test = null;
-var url = "https://api.meetup.com/2/open_events.xml?topic=photo&time=,1w&key=405727146c5e62374a697e641e27d";
+var url = "https://www.api.meetup.com/2/open_events.json?topic=photo&time=,1w&key=405727146c5e62374a697e641e27d";
 
 
 $(document).ready(function() {	
