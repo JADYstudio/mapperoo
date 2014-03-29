f72882ad88abee84c109b408405f86a69782c587
BunchaNumbers <jacobhuangyuning@msn.com>
2014-03-29 05:26:29 -0400

    Derf plis

--- meetup_getApi.js
+++ meetup_getApi.js
@@ -1,15 +1,16 @@
 // JavaScript Document
-var APIKEY = "405727146c5e62374a697e641e27d";
-var results = [];
+var APIKEY = "26157830757d475e4b557d7e5d725b35";
 
-function test(lat, lon){
+function test(lat, lon, back_fn){
 	
 var query = "https://api.meetup.com/2/open_events?callback=?&lat="+lat+"&lon="+lon+"&radius=smart&key="+APIKEY;
+
+var results = [];
 
 $.getJSON(query, function (data) {
 		$.each(data.results, function (i, item) {
 			results [i] = item; 
 		});
+		back_fn(results);
     });	
-	return results;
 }
