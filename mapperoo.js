322a1cf53fe42dfaa46ae63d1243639545f419a1
BunchaNumbers <jacobhuangyuning@msn.com>
2014-03-29 12:22:04 -0400

    trying to debug

--- mapperoo.js
+++ mapperoo.js
@@ -109,6 +109,7 @@ function initialize() {
 	// Create the search box and link it to the UI element.
 	var input = /** @type {HTMLInputElement} */(
 		document.getElementById('search-bar'));
+		console.log(input);
 	//map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
 	var searchBox = new google.maps.places.SearchBox(
